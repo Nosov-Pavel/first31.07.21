b7dbcea0e506b0684eaf549fd93e23f7aa0c2525
Nosov-Pavel <nopaev@gmail.com>
2021-08-01 08:13:37 -0400

    table

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello PASV!</h1>
+        <p>some text here</p>
+
+        <h2>Subtitle</h2>
+
+        <table border={1}>
+          <tr>
+            <td>Name</td>
+            <td>Price</td>
+            <td>Count</td>
+          </tr>
+          <tr>
+            <td>Box 123</td>
+            <td>$44456</td>
+            <td>21</td>
+          </tr>
+
+
+        </table>
+
       </header>
     </div>
   );
