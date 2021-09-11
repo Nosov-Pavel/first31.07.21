6d89cc6ea52dc1036f22d92eccd58be4552900ac
Nosov-Pavel <nopaev@gmail.com>
2021-09-11 11:14:47 -0400

    useState

---
+++ src/Ap.js
@@ -0,0 +1,13 @@
+import {useState} from "react";
+
+function Ap(){
+    const [count, setCount] = useState(5)
+
+    return(
+        <div>
+            <h1>Use State</h1>
+
+        </div>
+    );
+}
+export default Ap;
