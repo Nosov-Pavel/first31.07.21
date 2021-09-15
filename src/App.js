79af54cbfdc7983fb8dded5ca51e3cf85129b1ce
Nosov-Pavel <nopaev@gmail.com>
2021-09-14 21:32:10 -0400

    useStateIndex

--- src/App.js
+++ src/App.js
@@ -1,62 +1,45 @@
+
 import {useState} from "react";
 
 function App(){
-    const [count, setCount] = useState(108)
+    const [count, setCount] = useState(5)
+
     const plusOne = (digit) => {
         setCount(count + digit)
     }
+    const [counters, setCounters] = useState([1, 2, 3, ])
 
-    return(
-        <div>
-            <h1>UseState</h1>
-            <button onClick={() => plusOne(-1)}>-1</button>
-            {count}
-            <button onClick={() => plusOne(1)}>+1</button>
+    const plusMinusButtonHandler = (digit, index) => {
+        const newCounters = counters.map((el, ind) => ind === index ? el + digit : el)
+        setCounters(newCounters)
+    }
 
-        </div>
-    );
+  return(
+      <div>
+        <h1>Use State</h1>
+
+          <p>
+              <button onClick={() => plusOne(-1)}> -1 </button>
+              {count}
+              <button onClick={() => plusOne(1)}> +1 </button>
+          </p>
+
+          <hr/>
+          {counters.map((el, index) =>
+          <div>
+              <button onClick={() => plusMinusButtonHandler(-1, index)}> -1 </button>
+              {el}
+              <button onClick={() => plusMinusButtonHandler(+1, index)}> +1 </button>
+          </div>
+          )}
+          <hr/>
+
+
+       </div>
+  );
 }
-export default App;
-// import {useState} from "react";
-//
-// function App(){
-//     const [count, setCount] = useState(5)
-//
-//     const plusOne = (digit) => {
-//         setCount(count + digit)
-//     }
-//     const [counters, setCounters] = useState([1, 2, 3, ])
-//
-//     const plusMinusButtonHandler = () => {
-//
-//     }
-//
-//   return(
-//       <div>
-//         <h1>Use State</h1>
-//
-//           <p>
-//               <button onClick={() => plusOne(-1)}> -1 </button>
-//               {count}
-//               <button onClick={() => plusOne(1)}> +1 </button>
-//           </p>
-//
-//           <hr/>
-//           {counters.map((el, index) =>
-//           <div>
-//               <button onClick={() => plusMinusButtonHandler(-1)}> -1 </button>
-//               {el}
-//               <button onClick={() => plusMinusButtonHandler(+1)}> +1 </button>
-//           </div>
-//           )}
-//           <hr/>
-//
-//
-//        </div>
-//   );
-// }
 
-// export default App;
+export default App;
 
 
 
