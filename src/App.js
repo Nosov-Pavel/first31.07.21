15862f3ee01f882f76573de7170909bbc1013049
Nosov-Pavel <nopaev@gmail.com>
2021-09-15 22:06:11 -0400

    useStateAgain

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-
-import {useState} from "react";
-
 function App(){
-    const [count, setCount] = useState(5)
 
-    const plusOne = (digit) => {
-        setCount(count + digit)
-    }
-    const [counters, setCounters] = useState([1, 2, 3, ])
-
-    const plusMinusButtonHandler = (digit, index) => {
-        const newCounters = counters.map((el, ind) => ind === index ? el + digit : el)
-        setCounters(newCounters)
-    }
 
   return(
       <div>
-        <h1>Use State</h1>
-
-          <p>
-              <button onClick={() => plusOne(-1)}> -1 </button>
-              {count}
-              <button onClick={() => plusOne(1)}> +1 </button>
-          </p>
-
-          <hr/>
-          {counters.map((el, index) =>
-          <div>
-              <button onClick={() => plusMinusButtonHandler(-1, index)}> -1 </button>
-              {el}
-              <button onClick={() => plusMinusButtonHandler(+1, index)}> +1 </button>
-          </div>
-          )}
-          <hr/>
-
+          <h1>UseState</h1>
 
        </div>
   );
