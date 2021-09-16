bcafb1fad6c01a26fa21d4f24343164d100f0b49
Nosov-Pavel <nopaev@gmail.com>
2021-09-16 06:28:23 -0400

    digit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import {useState} from "react";
+
 function App(){
+    const [counter, setCounter] = useState(1);
+
+    const mathAction = (digit) => {
+        setCounter(counter + digit)
+        
+    }
+
 
 
   return(
       <div>
           <h1>UseState</h1>
+          <button onClick={() => mathAction(-1)}>-1</button>
+          {counter}
+          <button onClick={() => mathAction(+1)}>+1</button>
 
        </div>
   );
