2c92403524e47241a76ad6896b1914d10ecd0b7e
Nosov-Pavel <nopaev@gmail.com>
2021-09-16 13:04:33 -0400

    mathAction

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
+// import {useState} from "react";
+//
+// function App() {
+//     const [counter, setCounter] = useState(1)
+//
+//     const mathAction = (digit) => {
+//         setCounter(counter + digit)
+//     }
+//
+//     const [counters, setCounters] = useState([1, 2, 3])
+//
+//     const plMinButtonHandler = () => {
+//
+//     }
+//
+//
+//     return (
+//         <div>
+//             <h1>SetCounter</h1>
+//             {counters.map((el, index) =>
+//                 <div>
+//                     <button onClick={() => plMinButtonHandler(-1, index)}>-1</button>
+//                     {el}
+//                     <button onClick={() => plMinButtonHandler(+1, index)}>+1</button>
+//                 </div>
+//             )}
+//             <hr/>
+//             <button onClick={() => mathAction(-1)}>-1</button>
+//             {counter}
+//             <button onClick={() => mathAction(+1)}>+1</button>
+//
+//         </div>
+//     );
+// }
+//
+// export default App
+
 import {useState} from "react";
 
-function App() {
-    const [counter, setCounter] = useState(1)
+function App(){
+
+    const [counter, setCounter] = useState(10)
 
     const mathAction = (digit) => {
         setCounter(counter + digit)
-    }
-
-    const [counters, setCounters] = useState([1, 2, 3])
-
-    const plMinButtonHandler = () => {
 
     }
 
-
-    return (
+    return(
         <div>
             <h1>SetCounter</h1>
-            {counters.map((el, index) =>
-                <div>
-                    <button onClick={() => plMinButtonHandler(-1, index)}>-1</button>
-                    {el}
-                    <button onClick={() => plMinButtonHandler(+1, index)}>+1</button>
-                </div>
-            )}
-            <hr/>
             <button onClick={() => mathAction(-1)}>-1</button>
             {counter}
             <button onClick={() => mathAction(+1)}>+1</button>
@@ -32,10 +56,7 @@ function App() {
         </div>
     );
 }
-
-export default App
-
-
+export default App;
 
 
 
