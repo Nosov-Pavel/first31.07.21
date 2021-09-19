65c4b61ddacd7ef8576158b8241bfeae3c847e2e
Nosov-Pavel <nopaev@gmail.com>
2021-09-19 17:43:58 -0400

    counterLi

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
 import {useState} from "react";
 
 function App() {
-    const [counter, setCounter] = useState(1)
+    const initialCounter = [0, 0, 0];
 
-    const mathAction = (digit) => {
-        setCounter(counter + digit)
-    }
+    const [counters, setCounter] = useState(initialCounter)
 
-    const [counters, setCounters] = useState([1, 2, 3])
 
-    const plMinButtonHandler = () => {
-
-    }
 
 
     return (
         <div>
-            <h1>SetCounter</h1>
-            {counters.map((el, index) =>
-                <div key={index}>
-                    <button onClick={() => plMinButtonHandler(-1, index)}>-1</button>
-                    {el}
-                    <button onClick={() => plMinButtonHandler(+1, index)}>+1</button>
-                </div>
-            )}
-            <hr/>
-            <button onClick={() => mathAction(-1)}>-1</button>
-            {counter}
-            <button onClick={() => mathAction(+1)}>+1</button>
+            <li>
+            {counters.map(el => el)}
+            </li>
+
 
         </div>
     );
