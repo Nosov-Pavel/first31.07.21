60f72de481e133932300c0762e433aab95b13527
Nosov-Pavel <nopaev@gmail.com>
2021-09-22 06:54:52 -0400

    wrong

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import {useState} from "react";
 
 function App(){
-    const inicialCounters = [0, 0, 0];
-    const [counters,setCounters] = useState(inicialCounters);
+    const iCounters = [0, 0, 0];
+    const [counters,setCounters] = useState(iCounters);
 
     const addCounter = () => {
         const newCounters = [...counters];
         newCounters.push(0)
         setCounters(newCounters)
-
+    }
+    const delCounter = (index) => {
+        const newCounter = counters.filter((el, i) => i !== index)
+        setCounters(newCounter)
     }
 
     return(
         <div>
             <button onClick={addCounter}>AddCounter</button>
-            {counters.map(el => <li>
+            {counters.map((el, i) => <li>
             <button>+</button>
             {el}
             <button>-</button>
             </li>)}
+            <button onClick={()=>delCounter(i)}>DEL</button>
         </div>
     );
 }
